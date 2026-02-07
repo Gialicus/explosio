ff57d1b89535c5a288ad939e9e95aa6638d13217
Gialicus <dev@tomuki.com>
2026-02-07 16:15:17 +0100

    reset

--- main.go
+++ main.go
@@ -1,34 +1,7 @@
 package main
 
-import (
-	"explosio/lib"
-	"explosio/ui"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-)
+import "fmt"
 
 func main() {
-	myApp := app.NewWithID("com.explosio.app")
-	window := myApp.NewWindow("Explosio")
-	window.Resize(fyne.NewSize(800, 600))
-	window.CenterOnScreen()
-
-	project := lib.NewProject()
-	simulazioneContainer := container.NewStack()
-	refreshSimulazione := func() {
-		simulazioneContainer.RemoveAll()
-		simulazioneContainer.Add(ui.NewSimulationTab(project))
-		simulazioneContainer.Refresh()
-	}
-	refreshSimulazione()
-	progettoContent := ui.NewProjectTab(project, refreshSimulazione)
-	tabs := container.NewAppTabs(
-		container.NewTabItem("Progetto", progettoContent),
-		container.NewTabItem("Simulazione", simulazioneContainer),
-	)
-	window.SetContent(tabs)
-
-	window.ShowAndRun()
+	fmt.Println("Hello, World!")
 }
